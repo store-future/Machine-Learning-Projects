b3249562f68eda48feac3d87b430a528dc066ee1
dhananjay <126044351+store-future@users.noreply.github.com>
2023-12-12 14:40:54 +0530

    uploaded recommendation system
    uploaded recommendation system

---
+++ recommendation_system/preactise.py
@@ -0,0 +1,21 @@
+# s = "dhaf"
+# print(id(s))
+# # s = "dhamn"
+# # print(id(s))
+
+
+# l = [1,2,4]
+# print(id(l))
+# # l = [1,2,3]
+# # pr
+
+
+
+x = [(9,2),(6,91),(7,1)]
+print(sorted(x , key = x[] ))
+# print(x[2][1])
+
+
+l=(2,6,1,54,7,5)
+print(l[2])
+# print(sorted(x))
