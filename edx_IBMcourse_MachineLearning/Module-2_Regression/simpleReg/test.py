5d4e8a52d5210e0bb53dd6fc026a97eab29f01af
dhananjay <126044351+store-future@users.noreply.github.com>
2024-06-05 18:25:35 +0530

    creating ibmCourse_machineLearning
    include regression module

---
+++ edx_IBMcourse_MachineLearning/Module-2_Regression/simpleReg/test.py
@@ -0,0 +1,10 @@
+import os
+
+#checking the working directory
+# file_path = "..\simpleReg\FuelConsumptionCo2.csv"
+# if os.path.exists(file_path):
+#     print("File exists")
+# else:
+#     print("File not found")
+
+print(os.getcwd())
